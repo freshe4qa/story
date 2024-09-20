90abcfc86aa3eca3a706fdf7c076500cec0bd843
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2024-09-20 15:04:28 +0300

    Update story.sh

--- story.sh
+++ story.sh
@@ -21,6 +21,7 @@ sleep 2
 PS3='Select an action: '
 options=(
 "Install"
+"Check your wallet"
 "Create Validator"
 "Exit")
 select opt in "${options[@]}"
@@ -33,15 +34,18 @@ echo "Install start"
 echo "============================================================"
 
 # set vars
-if [ ! $NODENAME ]; then
-	read -p "Enter node name: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+NODE="story"
+DAEMON_HOME="$HOME/.story/story"
+DAEMON_NAME="story"
+if [ -d "$DAEMON_HOME" ]; then
+    new_folder_name="${DAEMON_HOME}_$(date +"%Y%m%d_%H%M%S")"
+    mv "$DAEMON_HOME" "$new_folder_name"
 fi
-if [ ! $WALLET ]; then
-	echo "export WALLET=wallet" >> $HOME/.bash_profile
+
+if [ ! $VALIDATOR ]; then
+    read -p "Enter validator name: " VALIDATOR
+    echo 'export VALIDATOR='\"${VALIDATOR}\" >> $HOME/.bash_profile
 fi
-echo "export STORY_CHAIN_ID=iliad" >> $HOME/.bash_profile
-source $HOME/.bash_profile
 
 # update
 sudo apt update && sudo apt upgrade -y
@@ -50,39 +54,41 @@ sudo apt update && sudo apt upgrade -y
 apt install curl iptables build-essential git wget jq make gcc nano tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev lz4 aria2 pv -y
 
 # install go
-ver="1.23.1" && \
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz" && \
-sudo rm -rf /usr/local/go && \
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz" && \
-rm "go$ver.linux-amd64.tar.gz" && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile && \
-source $HOME/.bash_profile && \
+cd $HOME
+VERSION=1.23.0
+wget -O go.tar.gz https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go.tar.gz && rm go.tar.gz
+echo 'export GOROOT=/usr/local/go' >> $HOME/.bash_profile
+echo 'export GOPATH=$HOME/go' >> $HOME/.bash_profile
+echo 'export GO111MODULE=on' >> $HOME/.bash_profile
+echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile && . $HOME/.bash_profile
 go version
 
-ufw allow 30303 comment story_geth_p2p_port
-ufw allow 26656 comment story_p2p_port
-
 # download binary
-mkdir -p $HOME/go/bin/
 cd $HOME
-mkdir -p $HOME/story && cd story
-wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.2-ea9f0d2.tar.gz
-tar -xvf geth-linux-amd64-0.9.2-ea9f0d2.tar.gz
-mv $HOME/story/geth-linux-amd64-0.9.2-ea9f0d2/geth $HOME/go/bin/story-geth
-story-geth version
-rm -r geth-linux-amd64*
+sudo rm -rf story
+git clone https://github.com/piplabs/story
+cd story
+git checkout v0.10.0
+go build -o story ./client
+mv story $HOME/go/bin
 
-wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.10.0-9603826.tar.gz
-tar -xvf story-linux-amd64-0.10.0-9603826.tar.gz
-mv $HOME/story/story-linux-amd64-0.10.0-9603826/story $HOME/go/bin/story
-story version
-rm -r story-linux-amd64*
+cd $HOME
+rm -rf story-geth
+wget -O geth-linux-amd64-0.9.2-ea9f0d2.tar.gz https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.2-ea9f0d2.tar.gz 
+tar xvf geth-linux-amd64-0.9.2-ea9f0d2.tar.gz
+sudo chmod +x geth-linux-amd64-0.9.2-ea9f0d2/geth
+sudo mv geth-linux-amd64-0.9.2-ea9f0d2/geth /usr/local/bin/story-geth
 
 # init
-story init --moniker "$NODENAME" --network $STORY_CHAIN_ID
+story init --network iliad  --moniker "${VALIDATOR}"
+
+story validator export --export-evm-key --evm-key-path $HOME/.story/.env
+story validator export --export-evm-key >>$HOME/.story/story/config/wallet.txt
+cat $HOME/.story/.env >>$HOME/.story/story/config/wallet.txt
 
 # download genesis and addrbook
-wget -O $HOME/.story/story/config/addrbook.json "https://share102.utsa.tech/story/addrbook.json"
+wget -O $HOME/.story/story/config/addrbook.json https://raw.githubusercontent.com/McDaan/general/main/story/addrbook.json
 
 # set peers and seeds
 PEERS=$(curl -sS https://story-rpc.mandragora.io/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)
@@ -92,61 +98,110 @@ SEEDS=6a07e2f396519b55ea05f195bac7800b451983c0@story-seed.mandragora.io:26656,51
 sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/" $HOME/.story/story/config/config.toml
 
 # create service
-tee /etc/systemd/system/story-geth.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/story-geth.service > /dev/null <<EOF  
 [Unit]
-Description=Story Geth Client
-After=network.target
+Description=Story execution daemon
+After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$HOME/go/bin/story-geth --iliad --syncmode full --http --http.api eth,net,web3,engine --http.vhosts '*' --http.addr 127.0.0.1 --http.port 8545 --ws --ws.api eth,web3,net,txpool --ws.addr 127.0.0.1 --ws.port 8546
-Restart=on-failure
+#WorkingDirectory=$HOME/.story/geth
+ExecStart=/usr/local/bin/story-geth --iliad --syncmode full
+Restart=always
 RestartSec=3
-LimitNOFILE=65535
+LimitNOFILE=infinity
+LimitNPROC=infinity
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-tee /etc/systemd/system/story.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/$NODE.service > /dev/null <<EOF  
 [Unit]
-Description=Story Consensus Client
-After=network.target
+Description=Story consensus daemon
+After=network-online.target
 
 [Service]
 User=$USER
 WorkingDirectory=$HOME/.story/story
-ExecStart=$HOME/go/bin/story run
-Restart=on-failure
+ExecStart=/usr/local/bin/story run
+Restart=always
 RestartSec=3
-LimitNOFILE=65535
+LimitNOFILE=infinity
+LimitNPROC=infinity
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
+sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
+Storage=persistent
+EOF
+
 # reset
-cd $HOME
-rm -f Geth_snapshot.lz4
-wget --show-progress https://josephtran.co/Geth_snapshot.lz4
-rm -rf ~/.story/story/data
-rm -rf ~/.story/geth/iliad/geth/chaindata
-sudo mkdir -p /root/.story/story/data
-lz4 -d Story_snapshot.lz4 | pv | sudo tar xv -C /root/.story/story/
+sudo rm -rf $HOME/.story/geth/iliad/geth/chaindata
+sudo rm -rf $HOME/.story/story/data
+
+wget -O geth_snapshot.lz4 https://snapshots.mandragora.io/geth_snapshot.lz4
+wget -O story_snapshot.lz4 https://snapshots.mandragora.io/story_snapshot.lz4
+
+lz4 -c -d geth_snapshot.lz4 | tar -x -C $HOME/.story/geth/iliad/geth
+lz4 -c -d story_snapshot.lz4 | tar -x -C $HOME/.story/story
+
+sudo rm -v geth_snapshot.lz4
+sudo rm -v story_snapshot.lz4
+
+#CHECK PORTS
+PORT=335
+if ss -tulpen | awk '{print $5}' | grep -q ":26656$" ; then
+    echo -e "\e[31mPort 26656 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:26656\"|:${PORT}56\"|g" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26656 changed to ${PORT}56.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":26657$" ; then
+    echo -e "\e[31mPort 26657 already in use\e[39m"
+    sleep 2
+    sed -i -e "s|:26657\"|:${PORT}57\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26657 changed to ${PORT}57.\e[0m\n"
+    sleep 2
+    #$DAEMON_NAME config node tcp://localhost:${PORT}57
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":26658$" ; then
+    echo -e "\e[31mPort 26658 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:26658\"|:${PORT}58\"|" $DAEMON_HOME/config/config.toml
+    echo -e "\n\e[42mPort 26658 changed to ${PORT}58.\e[0m\n"
+    sleep 2
+fi
+if ss -tulpen | awk '{print $5}' | grep -q ":1317$" ; then
+    echo -e "\e[31mPort 1317 already in use.\e[39m"
+    sleep 2
+    sed -i -e "s|:1317\"|:${PORT}17\"|" $DAEMON_HOME/config/story.toml
+    echo -e "\n\e[42mPort 1317 changed to ${PORT}17.\e[0m\n"
+    sleep 2
+fi
 
 # start service
-systemctl daemon-reload
-systemctl enable story
-systemctl enable story-geth
+sudo systemctl restart systemd-journald
+sudo systemctl daemon-reload
+sudo systemctl enable $NODE
+sudo systemctl restart $NODE
+sudo systemctl enable story-geth
+sudo systemctl restart story-geth
+
+break
+;;
+
+"Check your wallet")
+story validator export | grep "EVM Public Key:" | awk '{print $NF}'
 
 break
 ;;
 
 "Create Validator")
-story validator export \
-story validator export --export-evm-key \
-story validator export --export-evm-key --evm-key-path $HOME/.story/story/.env \
-cat /root/.story/story/config/private_key.txt \
+cd $HOME/.story
 story validator create --stake 1000000000000000000
   
 break
